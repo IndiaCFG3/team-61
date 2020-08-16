2007c4f8b52596097794cc0d53038b611c36b6b7
Rohit Pai <rohit23110@gmail.com>
2020-08-16 16:57:32 +0530

    User and Schemes Schema and APIs

--- routes/api/users.js
+++ routes/api/users.js
@@ -27,10 +27,15 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
+      const incomeRangeObject = ;
+      
       const newUser = new User({
         name: req.body.name,
+        username: req.body.username,
         email: req.body.email,
         password: req.body.password,
+        phone: req.body.phone,
+        gender: req.body.gender
       });
       // Hash password before saving in database
       bcrypt.hash(newUser.password, 12).then((hashed) => {
@@ -43,65 +48,147 @@ router.post("/register", (req, res) => {
     }
   });
 });
-// @route POST /users/login
-// @desc Login user and return JWT token
-// @access Public
-// router.post("/login", async (req, res) => {
-//   // Form validation
-//   try {
-//     const { errors, isValid } = validateLoginInput(req.body);
-//     // Check validation
-//     if (!isValid) {
-//       return res.status(400).json(errors);
-//     }
-//     const email = req.body.email;
-//     const password = req.body.password;
-//     // Find user by email
-//     var user = User.findOne({ email: req.body.email }).exec();
-//     // Check if user exists
-//     if (!user) {
-//       return res.status(404).json({ emailnotfound: "Email not found" });
-//     }
-//     // Check password
-//     if (!Bcrypt.compareSync(request.body.password, user.password))
-//       return res
-//         .status(404)
-//         .json({ passwordnotmatch: "The password is invalid" });
-//     response.send({
-//       message: "The username and password combination is correct!",
-//     });
-//   } catch (error) {
-//     return response.status(404);
-//   }
-// });
-// //   bcrypt.compare(password, user.password).then((isMatch) => {
-// //       if (isMatch) {
-// //         // User matched
-// //         // Create JWT Payload
-// //         const payload = {
-// //           id: user.id,
-// //           name: user.name,
-// //         };
-// //         // Sign token
-// //         jwt.sign(
-// //           payload,
-// //           keys.secretOrKey,
-// //           {
-// //             expiresIn: 31556926, // 1 year in seconds
-// //           },
-// //           (err, token) => {
-// //             res.json({
-// //               success: true,
-// //               token: "Bearer " + token,
-// //             });
-// //           }
-// //         );
-// //       } else {
-// //         return res
-// //           .status(400)
-// //           .json({ passwordincorrect: "Password incorrect" });
-// //       }
-// //     });
-// //   });
-// // });
+
 module.exports = router;
+
+  email: {
+    type: String,
+    required: false
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  incomeRange: {
+    "<1 Lakh": Boolean,
+    "1 - 3 Lakh": Boolean,
+    "3 - 5 Lakh": Boolean,
+    "5> Lakh": Boolean,
+    required: true, 
+  },
+  maritalStatus: {
+    "married": Boolean,
+    "unmarried": Boolean,
+    "widow/widower": Boolean,
+    required: true
+  },
+  educationStatus: {
+    "10th Pass": Boolean,
+    "12th Pass": Boolean,
+    "Undergraduate": Boolean,
+    "Graduate and above": Boolean,
+    "None of the above": Boolean,
+    required: true,
+  },
+  reservationStatus: {
+    "General": Boolean,
+    "SC": Boolean,
+    "ST": Boolean,
+    "OBC": Boolean,
+    "EWC": Boolean,
+    required: true
+  },
+  personWithDisability: {
+    type: Boolean,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },  
+  state: {
+    type: String,
+    required: true,
+  },
+  urbanRural: {
+    urban: Boolean,
+    rural: Boolean,
+    required: true
+  },
+  memberVerificationStatusPayment: {
+    type: Boolean,
+    required: true,
+  },
+  registrationTimestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  appliedSchemes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: appliedSchemesSchema,
+  }
+});
+
+const SchemeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  centralOrState: {
+    central: Boolean,
+    state: Boolean,
+    required: true
+  },
+  qualifiedStates: {
+    type: Object,
+    required: true
+  },
+  documentsRequired: {
+    type: Object,
+    required: true,
+  },
+  conditionsRequired: {
+    gender: {
+      type: Boolean,
+      required: true
+    },
+    isIncomeMoreThan5L: {
+      type: Boolean,
+      required: true
+    },
+    maritalStatus: {
+      type: String,
+      required: true
+    },
+    educationStatus: {
+      type: String,
+      required: true,
+    },
+    reservationStatus: {
+      type: String,
+      required: true
+    },
+    personWithDisability: {
+      type: Boolean,
+      required: true,
+    },
+    urbanRural: {
+      urban: Boolean,
+      rural: Boolean,
+      required: true
+    },
+  }
+});
+
+appliedScheme = new mongoose.Document({
+  schemeID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Scheme"
+  },
+  applicationStatus: {
+    notApplied: Boolean,
+    documentsNotVerified: Boolean,
+    applied: Boolean,
+    required: true
+  }
+})
+
+
