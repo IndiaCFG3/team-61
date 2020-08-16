62b923002a2615e6d49fee779847bfcb2e6c5fde
unvieux <57018679+UnVieuX@users.noreply.github.com>
2020-08-16 15:35:05 +0530

    .

--- App/Getting Request .py
+++ App/Getting Request .py
@@ -1,4 +1,12 @@
- response = requests.get(url, params=params)
+ 
+
+import requests
+import json
+from tkinter import *
+from tkinter.messagebox import showinfo, showerrorresponse = requests.get(url, params=params)
+
+
+
     dic = response.json()
     print(dic)
     return dic.get('return')
